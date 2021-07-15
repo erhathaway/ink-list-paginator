dbdfcd375aa65fdeb4c3b64e165a3f6267f3e86d
Ethan Hathaway <erhathaway@gmail.com>
2021-07-15 14:53:07 -0700

    Feat: add initial files

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # ink-list-paginator
+
 A list pagination component for React Ink. Works out of the box with ink-table
+
+# Install
+
+```shell
+npm run --save ink-list-paginator
+```
+
+
+This component is very simple. You may want to just copy the contents of [src/index.tsx](./src/index.tsx) instead of installing it. 
+
+The only dependencies are the two peer dependencies: `React` and `React Ink`
+
+# Usage
+
+The component works using the function as child (FACC) pattern.
+
+```tsx
+import PaginateList from 'ink-list-paginator'
+
+<PaginatedList
+    list={tableData}
+    pageSize={22}
+    isCursorOn={!isInInteractiveMode && mode === 'dataView'}
+>
+    {({data}) => (
+        <Table data={data} />
+    )}
+</PaginatedList>
+
+```
+
+
+The `PaginatedList` component takes 3 props:
+
+```ts
+    list: ListItem[];
+    pageSize?: number; // defaults to 100
+    isCursorOn: boolean;
+```
+
+- `list` is any list of data
+- `pageSize` is optional. It is the size of the page to be maintained
+- `isCursorOn` is a flag to tell if the component is in view and the user can interact with it. If `true` the list can be paginated incrementally using the `up` and `down` arrow keys, or paginated by page using the `pgup` and `pgdn` keys.
+
+
+
+The child function has the type signature:
+
+```
+({
+    data: ListItem[];
+    pageSize?: number;
+    isCursorOn: boolean;
+    pageRange: [number, number];
+}) => JSX.Element;
+```
---
+++ src/index.tsx
@@ -0,0 +1,96 @@
+import {useEffect, useState} from 'react';
+import {useInput} from 'ink';
+
+export type ListProps<ListItem> = {
+    list: ListItem[];
+    pageSize?: number;
+    isCursorOn: boolean;
+    children: ({
+        data,
+        pageSize,
+        isCursorOn,
+        pageRange
+    }: {
+        data: ListItem[];
+        pageSize?: number;
+        isCursorOn: boolean;
+        pageRange: [number, number];
+    }) => JSX.Element;
+};
+
+const movePageRangeClosure =
+    (pageRange: [number, number], totalSetSize: number, pageSize: number) =>
+    (movement: number): [number, number] => {
+        const [start, end] = pageRange;
+        let newStart = start;
+        let newEnd = end;
+
+        // apply changes
+        newStart = start + movement;
+        newEnd = end + movement;
+
+        // make sure 'start' isn't greater than last page
+        if (newStart > totalSetSize - pageSize) {
+            newStart = totalSetSize - pageSize;
+        }
+
+        // make sure 'start' isn't less than first page
+        if (newStart < 0) {
+            newStart = 0;
+        }
+
+        // make sure 'end' isn't greater than last page
+        if (newEnd > totalSetSize - 1) {
+            newEnd = totalSetSize - 1;
+        }
+
+        // make sure 'end' isn't less than first page
+        if (newEnd < pageSize) {
+            newEnd = pageSize - 1;
+        }
+
+        // make sure 'start' and 'end' are within set limits
+        if (pageSize > totalSetSize) {
+            newStart = 0;
+            newEnd = totalSetSize - 1;
+        }
+
+        return [newStart, newEnd];
+    };
+
+export const PaginatedList = <ListItem extends any>({
+    list: _ls,
+    pageSize: _ps,
+    isCursorOn,
+    children
+}: ListProps<ListItem>): JSX.Element => {
+    const pageSize = _ps == null ? 100 : _ps;
+    const [pageRange, setPageRange] = useState<[number, number]>([0, pageSize - 1]);
+    const [paginatedListData, setPaginatedListData] = useState<ListItem[]>(_ls.slice(...pageRange));
+
+    useInput((_input, key) => {
+        if (!isCursorOn) {
+            return;
+        }
+
+        const movePageRange = movePageRangeClosure(pageRange, _ls.length, pageSize);
+
+        if (key.downArrow) {
+            setPageRange(movePageRange(1));
+        } else if (key.upArrow) {
+            setPageRange(movePageRange(-1));
+        } else if (key.pageDown) {
+            setPageRange(movePageRange(pageSize));
+        } else if (key.pageUp) {
+            setPageRange(movePageRange(-pageSize));
+        }
+    });
+
+    useEffect(() => {
+        const fn = () => setPaginatedListData(_ls.slice(...pageRange));
+        // TOOD add throttle here
+        fn();
+    }, pageRange);
+
+    return children({data: paginatedListData, pageRange, pageSize, isCursorOn});
+};
